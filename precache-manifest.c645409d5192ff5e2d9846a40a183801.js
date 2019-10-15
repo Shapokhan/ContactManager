0e18ba67a47bbe46b7de93dc41c01f81bdf061b6
LiaqatSaeed <liaqatsaeed007@gmail.com>
2019-10-15 18:40:34 +0500

    Updates

---
+++ precache-manifest.c645409d5192ff5e2d9846a40a183801.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2e6760e7fc6f96716beeb2e66ef4cfbc",
+    "url": "/ContactManager/index.html"
+  },
+  {
+    "revision": "f48652a0ea4d1187e1bf",
+    "url": "/ContactManager/static/css/2.9ef9e05a.chunk.css"
+  },
+  {
+    "revision": "2757088530c7f1988ab1",
+    "url": "/ContactManager/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "f48652a0ea4d1187e1bf",
+    "url": "/ContactManager/static/js/2.251bcb94.chunk.js"
+  },
+  {
+    "revision": "2757088530c7f1988ab1",
+    "url": "/ContactManager/static/js/main.50641c97.chunk.js"
+  },
+  {
+    "revision": "7ec8c5112a7ad5871333",
+    "url": "/ContactManager/static/js/runtime-main.c5cfd899.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/ContactManager/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/ContactManager/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/ContactManager/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/ContactManager/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/ContactManager/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
